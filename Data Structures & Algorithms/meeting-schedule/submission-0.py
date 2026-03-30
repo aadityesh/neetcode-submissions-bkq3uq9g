4b652cc834c86f35afecb44db28a5e506cc7573d
AADITYESH JAIN <aadityeshjain@yahoo.com>
2026-03-30 08:56:18 +0530

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.py
@@ -0,0 +1,22 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        
+        n = len(intervals)
+        res = [intervals[0]]
+        
+        for i in range(1, n):
+            if res[-1].end > intervals[i].start:
+                res[-1].start, res[-1].end = min(res[-1].start, intervals[i].start), max(res[-1].end, intervals[i].end)
+            else:
+                res.append(intervals[i])
+        
+        return res == intervals
+            
