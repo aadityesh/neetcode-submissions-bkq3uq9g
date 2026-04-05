f262c8bb3556753128d6ba4261e078b8d62dc581
AADITYESH JAIN <aadityeshjain@yahoo.com>
2026-04-05 23:48:12 +0530

    Add: reverse-bits - submission-1

---
+++ Data Structures & Algorithms/reverse-bits/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+
+    def reverseBits(self, n: int) -> int:
+
+        res = ""
+        while n:
+            res += str(n % 2)
+            n //= 2
+        
+        zeroes = "0" * (32 - len(res))
+        binary_n = res[::-1] + zeroes
+
+        res = 0
+        offset = 31
+
+        for bit in binary_n:
+            res += (2**offset) * int(bit)
+            offset -= 1
+
+        return res
+
