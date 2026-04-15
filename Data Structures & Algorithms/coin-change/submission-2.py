a8252a182b5dc7fc77041154b15de518b5a2c23c
AADITYESH JAIN <aadityeshjain@yahoo.com>
2026-04-15 10:23:43 +0530

    Add: coin-change - submission-2

---
+++ Data Structures & Algorithms/coin-change/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        
+        dp = {}
+        def fn(remaining):
+            
+            if remaining in dp: 
+                return dp[remaining]
+                
+            if remaining == 0: 
+                return 0
+    
+            res = float("inf")
+            for coin in coins:
+                if amount - coin >= 0:
+                    res = min(res, 1 + fn(remaining - coin))
+
+            dp[remaining] = res
+            return dp[remaining]
+        
+        res = fn(amount)
+        return -1 if res == float("inf") else res
