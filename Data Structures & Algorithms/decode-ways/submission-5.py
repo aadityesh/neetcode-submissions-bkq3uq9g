9a871f4a5c73f57ea09cfb3c99c3ac942c87b1f6
AADITYESH JAIN <aadityeshjain@yahoo.com>
2026-04-15 08:50:08 +0530

    Add: decode-ways - submission-5

---
+++ Data Structures & Algorithms/decode-ways/submission-5.py
@@ -0,0 +1,51 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+
+        # n = len(s)
+        # dp = [-1] * (n+1)
+        
+
+        # fn(i) -> possible way of s from i to n
+        # dp[i] -> same as fn(i)
+
+
+        # def fn(i):
+
+        #     if i == n: 
+        #         return 1
+            
+        #     if s[i] == "0": 
+        #         return 0
+            
+        #     if dp[i] != -1:
+        #         return dp[i]
+
+        #     lh, rh = 0, 0
+            
+        #     lh = fn(i + 1)
+            
+        #     if i+1 < n: 
+        #         if (s[i] == "1" or (s[i] == "2" and s[i+1] <= '6')):
+        #             rh = fn(i+2)
+
+        #     dp[i] = (lh + rh)
+        #     return dp[i]
+
+        # return fn(0)
+
+        n = len(s)
+        dp = [0] * (n)
+
+        if s[-1] > '0':
+            dp[-1] = 1
+        
+        if s[n-2] >= "1":
+            dp[n-2] = 1 + dp[-1]
+        
+        for i in range(n - 3, -1, -1):
+            dp[i] = dp[i+1] + dp[i+2]
+
+        return dp[0] 
+        
+
+        
